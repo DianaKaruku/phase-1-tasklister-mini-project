c2232cb990d1f662244930c2d318da24aa1173c6
Diana Karuku <diana.karuku@student.moringaschool.com>
2023-10-04 22:15:28 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let createTaskForm = document.getElementById("create-task-form");
+  let newTaskDescription = document.getElementById("new-task-description");
+  let taskList = document.getElementById("tasks");
+
+  createTaskForm.addEventListener("submit",function(e){
+    e.preventDefault();
+
+    let taskDescription = newTaskDescription.value;
+
+let listItem =document.createElement("li");
+listItem.textContent = taskDescription;
+
+taskList.appendChild(listItem);
+
+newTaskDescription.value = "";
+  });
 });
